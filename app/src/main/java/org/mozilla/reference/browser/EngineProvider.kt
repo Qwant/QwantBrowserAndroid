571583935f6843ba46c6a172d60b3325850cb0a1
p.bertran <p.bertran@qwant.com>
2020-10-14 10:51:32 +0200

    Fork download (to debug, should not use)

--- app/src/main/java/org/mozilla/reference/browser/EngineProvider.kt
+++ app/src/main/java/org/mozilla/reference/browser/EngineProvider.kt
@@ -7,13 +7,14 @@ package org.mozilla.reference.browser
 import android.content.Context
 import android.util.Log
 import mozilla.components.browser.engine.gecko.GeckoEngine
-import mozilla.components.browser.engine.gecko.fetch.GeckoViewFetchClient
+// import mozilla.components.browser.engine.gecko.fetch.GeckoViewFetchClient
 import mozilla.components.concept.engine.DefaultSettings
 import mozilla.components.concept.engine.Engine
 import mozilla.components.concept.fetch.Client
 import mozilla.components.feature.webcompat.WebCompatFeature
 import org.mozilla.geckoview.GeckoRuntime
 import org.mozilla.geckoview.GeckoRuntimeSettings
+import org.mozilla.reference.browser.downloads.GeckoViewFetchClient
 
 object EngineProvider {
 
---
+++ app/src/main/java/org/mozilla/reference/browser/downloads/FetchDownloadManager.kt
@@ -0,0 +1,119 @@
+package org.mozilla.reference.browser.downloads
+
+import mozilla.components.feature.downloads.manager.DownloadManager
+import mozilla.components.feature.downloads.manager.validatePermissionGranted
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import android.Manifest.permission.FOREGROUND_SERVICE
+import android.Manifest.permission.INTERNET
+import android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+import android.app.DownloadManager.ACTION_DOWNLOAD_COMPLETE
+import android.app.DownloadManager.EXTRA_DOWNLOAD_ID
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES.P
+import androidx.core.net.toUri
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import mozilla.components.browser.state.action.DownloadAction
+import mozilla.components.browser.state.state.content.DownloadState.Status
+import mozilla.components.browser.state.state.content.DownloadState
+import mozilla.components.browser.state.store.BrowserStore
+// import mozilla.components.feature.downloads.AbstractFetchDownloadService
+import mozilla.components.feature.downloads.AbstractFetchDownloadService.Companion.EXTRA_DOWNLOAD_STATUS
+import mozilla.components.feature.downloads.manager.onDownloadStopped
+// import mozilla.components.feature.downloads.ext.isScheme
+import kotlin.reflect.KClass
+
+
+internal val noop_qwant: onDownloadStopped = { _, _, _ -> }
+/**
+ * Handles the interactions with [AbstractFetchDownloadService].
+ *
+ * @property applicationContext a reference to [Context] applicationContext.
+ * @property service The subclass of [AbstractFetchDownloadService] to use.
+ */
+class FetchDownloadManager<T : AbstractFetchDownloadService>(
+        private val applicationContext: Context,
+        private val store: BrowserStore,
+        private val service: KClass<T>,
+        private val broadcastManager: LocalBroadcastManager = LocalBroadcastManager.getInstance(applicationContext),
+        override var onDownloadStopped: onDownloadStopped = noop_qwant
+) : BroadcastReceiver(), DownloadManager {
+
+    private var isSubscribedReceiver = false
+
+    override val permissions = if (SDK_INT >= P) {
+        arrayOf(INTERNET, WRITE_EXTERNAL_STORAGE, FOREGROUND_SERVICE)
+    } else {
+        arrayOf(INTERNET, WRITE_EXTERNAL_STORAGE)
+    }
+
+    /**
+     * Schedules a download through the [AbstractFetchDownloadService].
+     * @param download metadata related to the download.
+     * @param cookie any additional cookie to add as part of the download request.
+     * @return the id reference of the scheduled download.
+     */
+    override fun download(download: DownloadState, cookie: String): String? {
+        if (!download.isScheme(listOf("http", "https", "data", "blob"))) {
+            return null
+        }
+        validatePermissionGranted(applicationContext)
+
+        // The middleware will notify the service to start the download
+        // once this action is processed.
+        store.dispatch(DownloadAction.AddDownloadAction(download))
+
+        registerBroadcastReceiver()
+        return download.id
+    }
+
+    override fun tryAgain(downloadId: String) {
+        val download = store.state.downloads[downloadId] ?: return
+
+        val intent = Intent(applicationContext, service.java)
+        intent.putExtra(EXTRA_DOWNLOAD_ID, download.id)
+        applicationContext.startService(intent)
+
+        registerBroadcastReceiver()
+    }
+
+    /**
+     * Remove all the listeners.
+     */
+    override fun unregisterListeners() {
+        if (isSubscribedReceiver) {
+            broadcastManager.unregisterReceiver(this)
+            isSubscribedReceiver = false
+        }
+    }
+
+    private fun registerBroadcastReceiver() {
+        if (!isSubscribedReceiver) {
+            val filter = IntentFilter(ACTION_DOWNLOAD_COMPLETE)
+            broadcastManager.registerReceiver(this, filter)
+            isSubscribedReceiver = true
+        }
+    }
+
+    /**
+     * Invoked when a download is complete. Notifies [onDownloadStopped] and removes the queued
+     * download if it's complete.
+     */
+    override fun onReceive(context: Context, intent: Intent) {
+        val downloadID = intent.getStringExtra(EXTRA_DOWNLOAD_ID) ?: ""
+        val download = store.state.downloads[downloadID]
+        val downloadStatus = intent.getSerializableExtra(EXTRA_DOWNLOAD_STATUS)
+                as Status
+
+        if (download != null) {
+            onDownloadStopped(download, downloadID, downloadStatus)
+        }
+    }
+}
