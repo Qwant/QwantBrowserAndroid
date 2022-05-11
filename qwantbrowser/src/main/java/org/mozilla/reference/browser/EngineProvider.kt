2c77fc226023c360a8c8e07d791c8ba1eac5eeb7
p.bertran <p.bertran@qwant.com>
2022-05-11 13:17:27 +0200

    Removing extension work from this branch to keep only component updates

--- qwantbrowser/src/main/java/org/mozilla/reference/browser/EngineProvider.kt
+++ qwantbrowser/src/main/java/org/mozilla/reference/browser/EngineProvider.kt
@@ -5,7 +5,6 @@
 package org.mozilla.reference.browser
 
 import android.content.Context
-import android.util.Log
 import mozilla.components.browser.engine.gecko.GeckoEngine
 import mozilla.components.browser.engine.gecko.fetch.GeckoViewFetchClient
 import mozilla.components.concept.engine.DefaultSettings
@@ -14,7 +13,6 @@ import mozilla.components.concept.fetch.Client
 import mozilla.components.feature.webcompat.WebCompatFeature
 import org.mozilla.geckoview.GeckoRuntime
 import org.mozilla.geckoview.GeckoRuntimeSettings
-import org.mozilla.geckoview.WebExtension
 
 object EngineProvider {
 
@@ -39,19 +37,8 @@ object EngineProvider {
     fun createEngine(context: Context, defaultSettings: DefaultSettings): Engine {
         val runtime = getOrCreateRuntime(context)
 
-        Log.d("QWANT_BROWSER_EXTENSION", "engine creation")
-        runtime.webExtensionController
-            // .installBuiltIn("resource://android/assets/webext_trackingprotection/")
-            .ensureBuiltIn("resource://android/assets/webext_trackingprotection/", "qwantprivacypilot-internal-beta-01@qwant.com")
-            .accept({ extension: WebExtension? ->
-                    Log.d("QWANT_BROWSER_EXTENSION", "Qwant Extension installed: $extension")
-            }) { e: Throwable? ->
-                Log.e("QWANT_BROWSER_EXTENSION","Error registering Qwant WebExtension", e)
-            }
-
         return GeckoEngine(context, defaultSettings, runtime).also {
             WebCompatFeature.install(it)
-            // QwantWebExtFeature.install(it)
         }
     }
 
