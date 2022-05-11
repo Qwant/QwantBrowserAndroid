665fda7a912c4ca2a3956538c4300013bd64872e
p.bertran <p.bertran@qwant.com>
2022-05-11 16:18:25 +0200

    onboarding layout more responsive

--- qwantbrowser/src/main/java/com/qwant/android/webext/ABPRemovalActivity.kt
+++ qwantbrowser/src/main/java/com/qwant/android/webext/ABPRemovalActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import org.mozilla.reference.browser.R
+import org.mozilla.reference.browser.ext.components
 
 
 class ABPRemovalActivity : AppCompatActivity() {
@@ -11,6 +12,10 @@ class ABPRemovalActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.onboarding_layout)
         findViewById<Button>(R.id.onboarding_validate)?.setOnClickListener {
+            this.finish()
+        }
+        findViewById<Button>(R.id.onboarding_more)?.setOnClickListener {
+            components.useCases.tabsUseCases.addTab("http://www.qwant.com/extension_more_info", true)
             this.finish()
         }
     }
