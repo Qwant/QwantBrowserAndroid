6e9eda2e943efd8f64ee82e174698155bb7b0f6a
p.bertran <p.bertran@qwant.com>
2022-06-02 11:39:21 +0200

    ABP removal (and some strings changes)

--- qwantbrowser/src/main/java/com/qwant/android/webext/ABPRemovalActivity.kt
+++ qwantbrowser/src/main/java/com/qwant/android/webext/ABPRemovalActivity.kt
@@ -1,8 +1,12 @@
 package com.qwant.android.webext
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import org.mozilla.reference.browser.R
 import org.mozilla.reference.browser.ext.components
 
@@ -10,13 +14,34 @@ import org.mozilla.reference.browser.ext.components
 class ABPRemovalActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.onboarding_layout)
-        findViewById<Button>(R.id.onboarding_validate)?.setOnClickListener {
-            this.finish()
+        this.initOnboardingForABPRemoval()
+    }
+
+    fun initOnboardingForABPRemoval() {
+        findViewById<ImageView>(R.id.onboarding_image)?.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.adblock_vert_2_x))
+
+        findViewById<TextView>(R.id.onboarding_title)?.text = getString(R.string.onboarding_adblockplus_title)
+        findViewById<TextView>(R.id.onboarding_bullet_1)?.text = getString(R.string.onboarding_adblockplus_bullet_1)
+        findViewById<TextView>(R.id.onboarding_bullet_2)?.text = getString(R.string.onboarding_adblockplus_bullet_2)
+
+        findViewById<Button>(R.id.onboarding_validate).apply {
+            text = getString(R.string.onboarding_adblockplus_validate)
+            setOnClickListener {
+                finish()
+            }
         }
-        findViewById<Button>(R.id.onboarding_more)?.setOnClickListener {
-            components.useCases.tabsUseCases.addTab("http://www.qwant.com/extension_more_info", true)
-            this.finish()
+        findViewById<Button>(R.id.onboarding_more)?.apply{
+            text = getString(R.string.onboarding_adblockplus_more)
+            setOnClickListener {
+                components.useCases.tabsUseCases.addTab(getString(R.string.onboarding_adblockplus_more_url), true)
+                finish()
+            }
         }
+
+        findViewById<TextView>(R.id.onboarding_bullet_3)?.visibility = View.GONE
+        findViewById<TextView>(R.id.onboarding_text_top)?.visibility = View.GONE
+        findViewById<TextView>(R.id.onboarding_text_bottom)?.visibility = View.GONE
     }
 }
